25c23f45d844be178ae9308acb5ff092d77e48eb
Hritik kumar <83581212+hritik-6918@users.noreply.github.com>
2023-09-07 20:38:38 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container header__container">
         <h3>Hey!👋Connections, I'm</h3>
         <h1>H r i t i k   K u m a r</h1>
-        <h2 className="text-light">SOFTWARE ENGINEER|WEB DEVELOPER</h2>
+        <h2 className="text-light">SOFTWARE ENGINEER | WEB DEVELOPER</h2>
         <CTA />
         <a href="#contact" className="scroll__down">
           Let's Intro
